693b24bcf97859fd8d2f00047943f504efd228c4
Solomon Lee <solomonslee123@gmail.com>
2022-06-25 19:44:10 -0700

    added file for game panel with some variables defined for ping pong game

---
+++ gamePanel.java
@@ -0,0 +1,57 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class gamePanel extends JPanel implements Runnable {
+	
+	static final int game_width = 1000;
+	static final int game_height = (int)(game_width * (5/9));
+	static final Dimension screenSize = new Dimension(game_width, game_height);
+	static final int ball_diameter = 20;
+	static final int paddle_width = 25;
+	static final int paddle_height = 100;
+	
+	gamePanel() {
+		
+	}
+	
+	public void newBall() {
+		
+	}
+	
+	public void newPaddle() {
+		
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkCollision() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	public class AL extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+			
+		}
+		
+		public void keyReleased(KeyEvent e) {
+			
+		}
+	}
+}
+
